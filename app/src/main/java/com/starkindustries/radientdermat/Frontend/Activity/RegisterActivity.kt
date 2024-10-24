728a739171df695dd53936b570e1a901200c26c0
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2024-10-24 10:51:21 +0530

    initial commit

---
+++ app/src/main/java/com/starkindustries/radientdermat/Frontend/Activity/RegisterActivity.kt
@@ -0,0 +1,24 @@
+package com.starkindustries.radientdermat.Frontend.Activity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.starkindustries.radientdermat.R
+import com.starkindustries.radientdermat.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    lateinit var binding:ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
